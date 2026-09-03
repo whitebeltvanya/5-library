e62de2894d3b1b92219d8f24fc8b9ec7c05d5785
vanya <whitebeltvanya@gmail.com>
2026-09-03 13:54:24 +0300

    add library.py

---
+++ library.py
@@ -0,0 +1,21 @@
+books: dict[str, str]= {}
+
+books["A Game of Thrones"] = "George R.R. Martin"
+books["A Clash of Kings"]  = "George R.R. Martin"
+books["The Fellowship of the Ring"] ="J.R.R. Tolkien"
+books["Harry Potter and the Sorcerer's Stone"] ="J.K. Rowling"
+books["Moby-Dick"] = "Herman Melville"
+
+
+authors: set[str] = set()
+for author in books.values():
+      authors.add(author)
+
+print("Список всех книг:")
+for book in sorted(books):
+    print(f"Название книги:{book}")
+
+print("-"*40)
+print("Список авторов:")
+for author in sorted(authors):
+    print(f"Автор книг:{author}")
