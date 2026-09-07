158755f65202aeda5c9ff06c40078c8ad9fea97f
vanya <whitebeltvanya@gmail.com>
2026-09-07 13:44:45 +0300

    fix 2 after review: delete function, rewrite map and sorted cases

--- library.py
+++ library.py
@@ -1,8 +1,7 @@
-from functools import reduce
 import sys
 
 ARGSV_COUNT = 3
-COMMAND_TYPES = ["filter","sort"]
+ACTION_COMMANDS = ["filter","sort"]
 SORT_TYPES = ["author","book"]
 
 BOOKS = {
@@ -14,45 +13,33 @@ BOOKS = {
 'Воскресение': 'Лев Николаевич Толстой'
 }
 
-  
-def print_books(books_map): # type: ignore
-
-    result = reduce(lambda acc, book: acc + f"{book['Книга'].ljust(40)} | {book['Автор']}\n", books_map,"") # type: ignore
-
-    print("-"*70)
-    print("Книга".ljust(40), "| ", "Автор")
-    print("-"*70)
-    print(result)
-    print("-"*70)
-    
-# start programm
-if len(sys.argv) != ARGSV_COUNT :
+# input args
+if len(sys.argv) != ARGSV_COUNT:
     print("Ошибка аргументов командной строки.")
     exit()
 
-action: str      = str(sys.argv[1]).strip().lower()
-action_txt: str  = str(sys.argv[2]).strip()
+action: str      = str(sys.argv[1]).strip().lower() # action
+action_parm: str  = str(sys.argv[2]).strip() # action parameter
 
+if action not in ACTION_COMMANDS:
+    print(f"Ошибка в команде. Используйте {', '.join(ACTION_COMMANDS)}")
+    exit()
 
+if action == "sort" and action_parm not in SORT_TYPES:
+    print(f"Ошибка аргумента команды 'sort'. Используйте: {', '.join(SORT_TYPES)}")
+    exit()
+
+# filter or sort
 books = dict(BOOKS)
-books_mapped = map(lambda book: {'Книга': book[0], 'Автор': book[1]}, books.items()) # type: ignore
-
 match action:
     case "filter":
-        books_filtred = filter(lambda book: action_txt in book["Автор"], books_mapped) # type: ignore
-        print_books(books_filtred)
+        books_filtred = filter(lambda book: action_parm in book[0], books.items()) # type: ignore
+        books_out = list(map(lambda book: f"{book[0]} - {book[1]}", books_filtred)) # type: ignore
+        print(books_out)
     case "sort":
-        if action_txt in SORT_TYPES :
-            if action_txt == "author":
-                books_sorted = sorted(books_mapped, key = lambda book: book["Автор"]) # type: ignore
-                print_books(books_sorted)               
-            elif action_txt == "book":
-                books_sorted = sorted(books_mapped, key = lambda book: book["Книга"]) # type: ignore
-                print_books(books_sorted)
-            else:
-                print(f"Ошибка аргумента команды 'sort'. Используйте: {', '.join(SORT_TYPES)}")
-        else:
-            print(f"Ошибка аргумента команды 'sort'. Используйте: {', '.join(SORT_TYPES)}")
+        books_list = list(map(lambda book: f"{book[0]} - {book[1]}", books.items())) # type: ignore
+        colum_idx_sort = 1 if action_parm == "author" else 0    
+        books_out = sorted(books_list, key = lambda book: book.split("-")[colum_idx_sort]) # type: ignore
+        print(books_list)
     case _:
-        print(f"Ошибка в команде. Используйте {', '.join(COMMAND_TYPES)}")
-
+        print("Ошибка в команде")
